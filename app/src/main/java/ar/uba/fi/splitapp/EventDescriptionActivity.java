fc58ddeff34dd9681d9d317a873a8f0cd4fbf191
Xero-Hige <gaston.martinez.90@gmail.com>
2016-07-15 17:28:46 -0300

    Added event description activity

---
+++ app/src/main/java/ar/uba/fi/splitapp/EventDescriptionActivity.java
@@ -0,0 +1,23 @@
+package ar.uba.fi.splitapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class EventDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_description);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+
+}
