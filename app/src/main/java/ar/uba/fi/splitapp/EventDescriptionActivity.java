dea69d65831f7e927f22f8980a5cdfe2667593e5
Xero-Hige <gaston.martinez.90@gmail.com>
2016-07-24 00:43:02 -0300

    Added task status layout

--- app/src/main/java/ar/uba/fi/splitapp/EventDescriptionActivity.java
+++ app/src/main/java/ar/uba/fi/splitapp/EventDescriptionActivity.java
@@ -5,11 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.facebook.Profile;
 import com.github.aakira.expandablelayout.ExpandableLinearLayout;
+import com.pkmmte.view.CircularImageView;
 
 public class EventDescriptionActivity extends AppCompatActivity {
+
+    private ExpandableLinearLayout my_tasks;
+    private ExpandableLinearLayout all_tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +31,37 @@ public class EventDescriptionActivity extends AppCompatActivity {
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
 
-        ExpandableLinearLayout my_tasks = (ExpandableLinearLayout) findViewById(R.id.expandable_my_tasks);
+        addExpandables();
+
+        LinearLayout templates = (LinearLayout) findViewById(R.id.all_tasks_list);
+        LinearLayout templates2 = (LinearLayout) findViewById(R.id.my_tasks_list);
+        LayoutInflater inflater = getLayoutInflater();
+
+        for (int i = 1; i < 21; i++) {
+            View templateItem = inflater.inflate(R.layout.task_status_layout, null);
+
+            TextView text = (TextView) templateItem.findViewById(R.id.task_name);
+            text.setText("Tarea #" + i);
+
+            TextView date = (TextView) templateItem.findViewById(R.id.task_status);
+            date.setText("Pendiente");
+
+            CircularImageView profile = (CircularImageView) templateItem.findViewById(R.id.task_profile_pic);
+            FacebookManager.fillWithUserPic(Profile.getCurrentProfile().getId(), profile, getApplicationContext());
+
+            //templates.addView(templateItem);
+            templates2.addView(templateItem);
+        }
+        my_tasks.initLayout(true);
+        all_tasks.initLayout(true);
+    }
+
+    private void addExpandables() {
+        my_tasks = (ExpandableLinearLayout) findViewById(R.id.expandable_my_tasks);
         RelativeLayout expand = (RelativeLayout) findViewById(R.id.expand_my_task);
         expand.setOnClickListener(v -> my_tasks.toggle());
 
-        ExpandableLinearLayout all_tasks = (ExpandableLinearLayout) findViewById(R.id.expandable_all_tasks);
+        all_tasks = (ExpandableLinearLayout) findViewById(R.id.expandable_all_tasks);
         expand = (RelativeLayout) findViewById(R.id.expand_all_tasks);
         expand.setOnClickListener(v -> all_tasks.toggle());
     }
