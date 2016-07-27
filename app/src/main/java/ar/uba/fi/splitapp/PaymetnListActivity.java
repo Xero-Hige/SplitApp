0bde99726518b5216e234dad2a69dfd7db31572c
Xero-Hige <gaston.martinez.90@gmail.com>
2016-07-26 22:34:41 -0300

    Added Payment List Activity

---
+++ app/src/main/java/ar/uba/fi/splitapp/PaymetnListActivity.java
@@ -0,0 +1,13 @@
+package ar.uba.fi.splitapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PaymetnListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paymetn_list);
+    }
+}
