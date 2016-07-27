52703698b4070ff384b8181d11b09c94a7cb962c
Colo <apredazzi@gmail.com>
2016-07-27 19:08:11 -0300

    Tipos de pago mockeados pero todos diferentes

--- app/src/main/java/ar/uba/fi/splitapp/PaymentListActivity.java
+++ app/src/main/java/ar/uba/fi/splitapp/PaymentListActivity.java
@@ -28,6 +28,148 @@ public class PaymentListActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        // HARDCODEO DE MEDIOS DE PAGO
+
+        View payment = inflater.inflate(R.layout.payment_layout, null);
+
+        TextView name = (TextView) payment.findViewById(R.id.payment_name);
+        name.setText("Efectivo");
+
+        ImageView icon = (ImageView) payment.findViewById(R.id.payment_icon);
+        Glide.with(this.getApplicationContext()).load(R.drawable.cash).centerCrop().into(icon);
+
+        payment.setOnClickListener(v -> {
+            Toast toast = Toast.makeText(this, "Pagado", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show();
+            Bundle event_id_passed = getIntent().getExtras();
+            String id_event = "error";
+            if (event_id_passed != null) {
+                id_event = event_id_passed.getString("id");
+            }
+            if (id_event != "error"){
+                Intent backMain = new Intent(this, EventDescriptionActivity.class);
+                backMain.putExtra("id",id_event);
+                startActivity(backMain);
+            } else {
+                Intent backMain = new Intent(this, DebtActivity.class);
+                startActivity(backMain);
+            }
+
+
+        });
+
+        payments.addView(payment);
+
+
+
+
+        View payment2 = inflater.inflate(R.layout.payment_layout, null);
+
+        TextView name2 = (TextView) payment2.findViewById(R.id.payment_name);
+        name2.setText("Paypal");
+
+        ImageView icon2 = (ImageView) payment2.findViewById(R.id.payment_icon);
+        Glide.with(this.getApplicationContext()).load(R.drawable.paypal).centerCrop().into(icon2);
+
+        payment2.setOnClickListener(v -> {
+            Toast toast = Toast.makeText(this, "Pagado", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show();
+            Bundle event_id_passed = getIntent().getExtras();
+            String id_event = "error";
+            if (event_id_passed != null) {
+                id_event = event_id_passed.getString("id");
+            }
+            if (id_event != "error"){
+                Intent backMain = new Intent(this, EventDescriptionActivity.class);
+                backMain.putExtra("id",id_event);
+                startActivity(backMain);
+            } else {
+                Intent backMain = new Intent(this, DebtActivity.class);
+                startActivity(backMain);
+            }
+
+
+        });
+
+        payments.addView(payment2);
+
+
+
+        View payment3 = inflater.inflate(R.layout.payment_layout, null);
+
+        TextView name3 = (TextView) payment3.findViewById(R.id.payment_name);
+        name3.setText("Transferencia Bancaria");
+
+        ImageView icon3 = (ImageView) payment3.findViewById(R.id.payment_icon);
+        Glide.with(this.getApplicationContext()).load(R.drawable.trans_bancaria).centerCrop().into(icon3);
+
+        payment3.setOnClickListener(v -> {
+            Toast toast = Toast.makeText(this, "Pagado", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show();
+            Bundle event_id_passed = getIntent().getExtras();
+            String id_event = "error";
+            if (event_id_passed != null) {
+                id_event = event_id_passed.getString("id");
+            }
+            if (id_event != "error"){
+                Intent backMain = new Intent(this, EventDescriptionActivity.class);
+                backMain.putExtra("id",id_event);
+                startActivity(backMain);
+            } else {
+                Intent backMain = new Intent(this, DebtActivity.class);
+                startActivity(backMain);
+            }
+
+
+        });
+
+        payments.addView(payment3);
+
+
+
+
+
+        View payment4 = inflater.inflate(R.layout.payment_layout, null);
+
+        TextView name4 = (TextView) payment4.findViewById(R.id.payment_name);
+        name4.setText("Mercado Pago");
+
+        ImageView icon4 = (ImageView) payment4.findViewById(R.id.payment_icon);
+        Glide.with(this.getApplicationContext()).load(R.drawable.mercadopago).centerCrop().into(icon4);
+
+        payment4.setOnClickListener(v -> {
+            Toast toast = Toast.makeText(this, "Pagado", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show();
+            Bundle event_id_passed = getIntent().getExtras();
+            String id_event = "error";
+            if (event_id_passed != null) {
+                id_event = event_id_passed.getString("id");
+            }
+            if (id_event != "error"){
+                Intent backMain = new Intent(this, EventDescriptionActivity.class);
+                backMain.putExtra("id",id_event);
+                startActivity(backMain);
+            } else {
+                Intent backMain = new Intent(this, DebtActivity.class);
+                startActivity(backMain);
+            }
+
+
+        });
+
+        payments.addView(payment4);
+
+
+
+
+
+/*
+
+
 
         for (int i = 0; i < 6; i++) {
             View payment = inflater.inflate(R.layout.payment_layout, null);
@@ -59,7 +201,7 @@ public class PaymentListActivity extends AppCompatActivity {
             });
 
             payments.addView(payment);
-        }
+        }*/
 
     }
 
